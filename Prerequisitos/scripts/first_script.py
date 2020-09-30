6c915340923031258846d36712e928c72b115aab
Maykol Chang <vPancake7@users.noreply.github.com>
2020-09-30 15:58:16 -0500

    commit

--- Prerequisitos/scripts/first_script.py
+++ Prerequisitos/scripts/first_script.py
@@ -1,1 +1,2 @@
 print('Executing the script')
+
--- Prerequisitos/scripts/value_error.py
+++ Prerequisitos/scripts/value_error.py
@@ -3,8 +3,8 @@ while True:
     entero = int(input('Ingrese un entero:'))
     break
   except ValueError:
-  	print('No es un entero, vuelva a ingresarlo')
+    print('No es un entero, vuelva a ingresarlo')
   finally:
-  	print('Se ejecuta si se da un error o no siempre')
+    print('Se ejecuta si se da un error o no siempre')
 
 print('Convertido en entero ', entero)
